faecb2ccda5788efad362c65521b48cae8322725
tocchimittsu <arimagedon21@gmail.com>
2021-11-18 18:03:16 +0900

    修正

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import cv2
 import sys
 
+# medianBlur関数を実行する関数
+def exec_median_blur(img):
+    # medianBlur関数 : 画像をぼかす関数
+    # 第一引数(必須) : 多次元配列(numpy.ndarray)
+    # 第二引数(必須) : n x nの画素領域を指定する。
+    # 戻り値 : 多次元配列(numpy.ndarray)
+    return cv2.medianBlur(img, 5)
+
 # GaussianBlur関数を実行する関数
 def exec_gaussian_blur(img):
     # GaussianBlur関数 : 画像をぼかす関数
@@ -23,14 +31,14 @@ def exec_blur(img):
 # imreadについて : https://kuroro.blog/python/wqh9VIEmRXS4ZAA7C4wd/
 # 第一引数 : 画像のファイルパス
 # 戻り値 : 行 x 列 x 色の三次元配列(numpy.ndarray)が返される。
-img = cv2.imread("sample_gaussian_blur.jpg")
+img = cv2.imread("sample_median_blur.jpg")
 
 # 画像ファイルが正常に読み込めなかった場合、プログラムを終了する。
 if img is None:
     sys.exit("Could not read the image.")
 
 # hconcat : 画像を連結する関数
-imgs = cv2.hconcat([img, exec_gaussian_blur(img)])
+imgs = cv2.hconcat([img, exec_median_blur(img)])
 
 # imwrite : 画像の保存を行う関数
 # 第一引数 : 保存先の画像ファイル名
@@ -54,4 +62,4 @@ imgs = cv2.hconcat([img, exec_gaussian_blur(img)])
 # ]
 # ]
 # imwriteについて : https://kuroro.blog/python/i0tNE1Mp8aEz8Z7n6Ggg/
-cv2.imwrite('output_gaussian_blur.jpg', imgs)
+cv2.imwrite('output_median_blur.jpg', imgs)
